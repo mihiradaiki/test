5e9c38773385b66ada5e069f4b57a2173a7d17de
mihira <one-piece.p.d.ace@docomo.ne.jp>
2018-02-28 15:27:18 +0900

    2018/2/28_1

---
+++ script.js
@@ -0,0 +1,7 @@
+function menseki(otona,kodomo){
+    return (otona*500+kodomo*200)+"円です。";
+}
+
+document.write(menseki(2,4)+"<br>");
+document.write(menseki(1,5)+"<br>");
+document.write(menseki(3,7)+"<br>");
---
+++ testproject01/src/testproject01/helloworld.java
@@ -0,0 +1,11 @@
+package testproject01;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// ｈello ｗorld!の出力
+       System.out.println("こんにちわ！！");
+	}
+
+}
+
